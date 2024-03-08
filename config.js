4e8247155ef926db8ec3425a88c026aaee207f2d
Nguyen Ngoc Quyen Anh <nnquyenanh@gmail.com>
2024-03-08 11:47:34 +0700

    end

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Bé Dunggg';
-const giftUrl = 'https://nguyenngocquyenanh.github.io/Dumm/';
+const giftUrl = 'https://nguyenngocquyenanh.github.io/dumm/'; 
 const eventName = 'Chúc Mừng 8/3';
 const titleCard = 'Dành tặng cho bé iu của anh';
 const contentCard = 'Ngày Phụ nữ, anh  xin gửi đến bé iu của anh những lời chúc tốt đẹp nhất.Chúc em luôn được yêu thương và hạnh phúc trong mọi khoảnh khắc của cuộc sống , em xinh lắm nên phải cười nhiều lên nhé . Khi nào gặp nhau anh sẽ bù món quà lớn hơn ý nghĩa hơn cho bé <3'
